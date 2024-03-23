7d2573324e56c8296c2048c29612c973c7d21866
Ragul <ragultamilmani823@gmail.com>
2024-03-23 23:22:34 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using ToDoApp.ViewModel;
+
+namespace ToDoApp
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Model;
+using ToDoApp.MVVM;
+
+namespace ToDoApp.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public ObservableCollection<Todo> Todos { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem(), canexecute => { return true; });
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(),canexecute=> SelectedItem!=null);
+
+        private void DeleteItem()
+        {
+            Todos.Remove(SelectedItem);
+        }
+
+        private void AddItem()
+        {
+            Todos.Add(new Todo { Title="DefaultTask", Description="Defalut Description"});
+        }
+
+        private Todo _selectedItem;
+
+        public Todo SelectedItem
+        {
+            get { return _selectedItem; }
+            set { 
+                _selectedItem = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Todos = new ObservableCollection<Todo>();
+          /*  Todos.Add(new Todo { Title = "XXX", Description = "YYYYY" });
+            Todos.Add(new Todo { Title = "ZZZ", Description ="wfasvafsvdfv" });*/
+        }
+    }
+}
